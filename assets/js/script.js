b482742291f9b3e5014e885c5a8d83ec53fa39f5
Sophie McGee <sophiebmcgee@outlook.com>
2024-03-31 10:00:33 +0000

    Fix error in file path for background image in CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,5 @@ function startGame(difficulty) {
     // Function to trigger the start of the game once difficulty is selected
     document.getElementById('header').style.display = 'block'; // Show the header when game starts
     document.getElementById('footer').style.display = 'block'; // Show the footer when game starts
-    document.getElementById('game-selectiion').style.display = 'none'; // Hide the intro section when game starts
+    document.getElementById('game-selection').style.display = 'none'; // Hide the intro section when game starts
 }
