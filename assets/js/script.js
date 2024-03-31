5ed6cdd6e830735e2677a2b32ae8b66180b5f268
Sophie McGee <sophiebmcgee@outlook.com>
2024-03-31 09:50:02 +0000

    Update CSS and Javascript to display header and footer when the game mode is chosen, added css styling for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-document.getElementById()
+document.getElementById('easyMode').addEventListener('click', function() {
+    startGame('easy');
+});
+
+document.getElementById('hardMode').addEventListener('click', function() {
+    startGame('hard');
+});
+
+function startGame(difficulty) {
+    // Function to trigger the start of the game once difficulty is selected
+    document.getElementById('header').style.display = 'block'; // Show the header when game starts
+    document.getElementById('footer').style.display = 'block'; // Show the footer when game starts
+    document.getElementById('game-selectiion').style.display = 'none'; // Hide the intro section when game starts
+}
