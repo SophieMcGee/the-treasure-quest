5085d4c754050b513a62dfb0e8e35fb127822251
Sophie McGee <sophiebmcgee@outlook.com>
2024-03-31 15:58:49 +0000

    Remove logo from header and update background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-document.getElementById('easyMode').addEventListener('click', function() {
+document.getElementById('easyMode').addEventListener('click', function () {
     startGame('easy');
 });
 
-document.getElementById('hardMode').addEventListener('click', function() {
+document.getElementById('hardMode').addEventListener('click', function () {
     startGame('hard');
 });
 
 function startGame(difficulty) {
     // Function to trigger the start of the game once difficulty is selected
-    document.getElementById('header').style.display = 'block'; // Show the header when game starts
-    document.getElementById('footer').style.display = 'block'; // Show the footer when game starts
-    document.getElementById('game-selection').style.display = 'none'; // Hide the intro section when game starts
+    document.getElementById('header').style.display = 'flex'; 
+    document.getElementById('game-selection').style.display = 'none'; 
+    document.getElementById('quiz-container').style.display = 'block';
 }
