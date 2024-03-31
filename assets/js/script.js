f62f235275b66d7693dc11fbe8ce43af849668af
Sophie McGee <sophiebmcgee@outlook.com>
2024-03-31 16:27:54 +0000

    Add function to start the quiz once difficulty is chosen via loadQuiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,11 @@ document.getElementById('hardMode').addEventListener('click', function () {
     startGame('hard');
 });
 
-function startGame(difficulty) {
-    // Function to trigger the start of the game once difficulty is selected
-    loadQuiz(difficulty);
 
+//Function to start the quiz
+function loadQuiz(difficulty) {
+    startGame(difficulty);
     document.getElementById('header').style.display = 'flex'; //Code to display the header
     document.getElementById('game-selection').style.display = 'none'; //Code to show the quiz game questions
-    document.getElementById('quiz-container').style.display = 'block'; //Code to show the footer
-}
-
-function loadQuiz(difficulty) {
-    
+    document.getElementById('quiz-container').style.display = 'block'; //Code to show the footer 
 }
