5fb37647c6db16c895c61edb9999360ca7cb3a89
Sophie McGee <sophiebmcgee@outlook.com>
2024-04-01 11:07:54 +0000

    Change game background, move logo to header and change javascript so header always visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,5 @@ function endQuiz() {
 //Function to start the quiz
 function loadQuiz(difficulty) {
     startGame(difficulty);
-    document.getElementById('header').style.display = 'flex'; //Code to display the header
     document.getElementById('game-selection').style.display = 'none'; //Code to show the quiz game questions
-    document.getElementById('quiz-container').style.display = 'block'; //Code to show the footer 
 }
