5ca210c6ee681027d902a6eedbb8bbe5cc4ef524
Sophie McGee <sophiebmcgee@outlook.com>
2024-04-04 19:03:12 +0000

    Add hard questions to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,60 +207,304 @@ const questions = {
             options: ["the equator", "North Pole", "South Pole"]
         },
         {
-        question: "What is the part of the Earth north of the equator called?",
-        correctAnswerPosition: 2,
-        options: ["southern hemisphere", "up hemisphere", "northern hemisphere"]
-    },
-    {
-        question: "In what hemisphere would you find Australia?",
-        correctAnswerPosition: 0,
-        options: ["southern hemisphere", "Australian hemisphere", "northern hemisphere"]
-    },
-    {
-        question: "What tropic is an imaginary line in the southern hemisphere?",
-        correctAnswerPosition: 2,
-        options: ["Cancer", "Virgo", "Capricorn"]
-    },
-    {
-        question: "In which hemisphere would you find Europe?",
-        correctAnswerPosition: 2,
-        options: ["European hemisphere", "Arctic hemisphere", "northern hemisphere"]
-    },
-    {
-        question: "In which hemisphere would you find most of the Earth’s land?",
-        correctAnswerPosition: 2,
-        options: ["southern hemisphere", "Indian hemisphere", "northern hemisphere"]
-    },
-    {
-        question: "What is the northernmost point on Earth called?",
-        correctAnswerPosition: 2,
-        options: ["South Pole", "Poland", "North Pole"]
-    },
-    {
-        question: "In summer, what never sets at the North Pole?",
-        correctAnswerPosition: 2,
-        options: ["the Moon", "the equator", "the Sun"]
-    },
-    {
-        question: "Where on Earth are days and nights both 12 hours long?",
-        correctAnswerPosition: 0,
-        options: ["the equator", "Australia", "South Pole"]
-    },
-    {
-        question: "What tropic is an imaginary line in the northern hemisphere?",
-        correctAnswerPosition: 2,
-        options: ["Taurus", "Scorpio", "Cancer"]
-    }
-],
-hard: [
-    {
-        question: "What is the capital city of England",
-        correctAnswerPosition: 2,
-        options: ["Dublin", "Paris", "London"]
-    }
-],
+            question: "What is the part of the Earth north of the equator called?",
+            correctAnswerPosition: 2,
+            options: ["southern hemisphere", "up hemisphere", "northern hemisphere"]
+        },
+        {
+            question: "In what hemisphere would you find Australia?",
+            correctAnswerPosition: 0,
+            options: ["southern hemisphere", "Australian hemisphere", "northern hemisphere"]
+        },
+        {
+            question: "What tropic is an imaginary line in the southern hemisphere?",
+            correctAnswerPosition: 2,
+            options: ["Cancer", "Virgo", "Capricorn"]
+        },
+        {
+            question: "In which hemisphere would you find Europe?",
+            correctAnswerPosition: 2,
+            options: ["European hemisphere", "Arctic hemisphere", "northern hemisphere"]
+        },
+        {
+            question: "In which hemisphere would you find most of the Earth’s land?",
+            correctAnswerPosition: 2,
+            options: ["southern hemisphere", "Indian hemisphere", "northern hemisphere"]
+        },
+        {
+            question: "What is the northernmost point on Earth called?",
+            correctAnswerPosition: 2,
+            options: ["South Pole", "Poland", "North Pole"]
+        },
+        {
+            question: "In summer, what never sets at the North Pole?",
+            correctAnswerPosition: 2,
+            options: ["the Moon", "the equator", "the Sun"]
+        },
+        {
+            question: "Where on Earth are days and nights both 12 hours long?",
+            correctAnswerPosition: 0,
+            options: ["the equator", "Australia", "South Pole"]
+        },
+        {
+            question: "What tropic is an imaginary line in the northern hemisphere?",
+            correctAnswerPosition: 2,
+            options: ["Taurus", "Scorpio", "Cancer"]
+        }
+    ],
+    hard: [
+        {
+            question: "Demographics are the statistical data of a population, showing characteristics such as?",
+            correctAnswerPosition: 2,
+            options: ["Income and age", "Education and income", "Income, age, and education"]
+        },
+        {
+            question: "Population density means how many people?",
+            correctAnswerPosition: 1,
+            options: ["Are educated", "Live per square km", "Have enough to eat"]
+        },
+        {
+            question: "Which one of these countries has the highest GDP per capita?",
+            correctAnswerPosition: 2,
+            options: ["Vietnam", "Egypt", "Luxembourg"]
+        },
+        {
+            question: "Which country has a high percentage of people living below the poverty line?",
+            correctAnswerPosition: 0,
+            options: ["Africa", "Europe", "North America"]
+        },
+        {
+            question: "What is the capital city of Spain?",
+            correctAnswerPosition: 0,
+            options: ["Madrid", "Vienna", "Rome"]
+        },
+        {
+            question: "Which city is home to the Eiffel Tower?",
+            correctAnswerPosition: 1,
+            options: ["Athens", "Paris", "London"]
+        },
+        {
+            question: "In which capital city would you find the Houses of Parliament and Buckingham Palace?",
+            correctAnswerPosition: 2,
+            options: ["Riga", "Stockholm", "London"]
+        },
+        {
+            question: "Vienna is the capital city of which country?",
+            correctAnswerPosition: 1,
+            options: ["Finland", "Austria", "Portugal"]
+        },
+        {
+            question: "What is the capital city of Iceland?",
+            correctAnswerPosition: 2,
+            options: ["Brussels", "Sofia", "Reykjavik"]
+        },
+        {
+            question: "In which country would you find the capital city of Copenhagen?",
+            correctAnswerPosition: 1,
+            options: ["Estonia", "Denmark", "Croatia"]
+        },
+        {
+            question: "Oslo is the capital city of which country?",
+            correctAnswerPosition: 1,
+            options: ["Sweden", "Norway", "Italy"]
+        },
+        {
+            question: "Berlin is the capital city of which European country?",
+            correctAnswerPosition: 2,
+            options: ["Serbia", "Switzerland", "Germany"]
+        },
+        {
+            question: "Which landmark in Ireland is known for granting the gift of eloquence?",
+            correctAnswerPosition: 0,
+            options: ["The Blarney Stone", "Cliffs of Moher", "Giant’s Causeway"]
+        },
+        {
+            question: "Which famous Irish attraction is associated with the iconic Irish beer?",
+            correctAnswerPosition: 2,
+            options: ["Trinity College", "Kilmainham Gaol", "Guinness Storehouse"]
+        },
+        {
+            question: "Which landmark in Ireland is also known as Cashel of the Kings and St. Patrick’s Rock?",
+            correctAnswerPosition: 0,
+            options: ["Rock of Cashel", "Ring of Kerry", "Dublin Castle"]
+        },
+        {
+            question: "Which natural wonder in Ireland consists of unique basalt columns?",
+            correctAnswerPosition: 1,
+            options: ["Kylemore Abbey", "Giants Causeway", "Blarney Castle"]
+        },
+        {
+            question: "Which of the following is not a country in North America?",
+            correctAnswerPosition: 2,
+            options: ["Canada", "Mexico", "Russia"]
+        },
+        {
+            question: "The United States of America has how many states?",
+            correctAnswerPosition: 1,
+            options: ["49", "50", "51"]
+        },
+        {
+            question: "Canada is the second largest country in the world but has a low?",
+            correctAnswerPosition: 1,
+            options: ["Amount of farming land", "Population density", "Number of animals"]
+        },
+        {
+            question: "The continent of Europe has almost how many countries?",
+            correctAnswerPosition: 2,
+            options: ["30", "40", "50"]
+        },
+        {
+            question: "Which of the following is not a country in Europe?",
+            correctAnswerPosition: 2,
+            options: ["Italy", "Portugal", "India"]
+        },
+        {
+            question: "Which European city does the prime Meridian run through?",
+            correctAnswerPosition: 0,
+            options: ["London", "Paris", "Madrid"]
+        },
+        {
+            question: "What is the capital city of Australia?",
+            correctAnswerPosition: 0,
+            options: ["Canberra", "Sydney", "Melbourne"]
+        },
+        {
+            question: "In which capital city would you find ‘The Forbidden City’?",
+            correctAnswerPosition: 1,
+            options: ["Tokyo", "Beijing", "Seoul"]
+        },
+        {
+            question: "Vilnius is the capital city of which European country?",
+            correctAnswerPosition: 2,
+            options: ["Latvia", "Estonia", "Lithuania"]
+        },
+        {
+            question: "The world’s highest mountain is Mount Everest in the Himalayas. What is the world’s second highest peak?",
+            correctAnswerPosition: 0,
+            options: ["K2 on the China-Pakistan border", "Kangchenjunga", "Lhotse"]
+        },
+        {
+            question: "Which freshwater lake in the Highlands of Scotland is the largest of all Scottish lakes?",
+            correctAnswerPosition: 0,
+            options: ["Loch Lomond", "Loch Ness", "Loch Morar"]
+        },
+        {
+            question: "Which river located in China is 3917 miles long and flows into the East China Sea?",
+            correctAnswerPosition: 1,
+            options: ["The Yellow River", "The Yangtze River", "The Pearl River"]
+        },
+        {
+            question: "Mount Everest is the world’s highest peak but what’s the name of the highest mountain in the solar system?",
+            correctAnswerPosition: 0,
+            options: ["Olympus Mons on Mars", "Rheasilvia on Vesta", "Maxwell Montes on Venus"]
+        },
+        {
+            question: "The UK’s longest river is 220 miles in length and runs through Shropshire, Worcestershire and Gloucestershire. What is its name?",
+            correctAnswerPosition: 1,
+            options: ["The River Thames", "The River Severn", "The River Trent"]
+        },
+        {
+            question: "The Scilly Isles are a group of islands situated off the UK coast. Which county are they nearest to?",
+            correctAnswerPosition: 0,
+            options: ["Cornwall", "Devon", "Somerset"]
+        },
+        {
+            question: "The prehistoric monument at Stonehenge is a World Heritage Site. Where in the UK is it?",
+            correctAnswerPosition: 2,
+            options: ["Dorset", "Somerset", "Wiltshire"]
+        },
+        {
+            question: "What is the name of the ridge of hills that run down the centre of northern England and are often called the ‘backbone’ of England?",
+            correctAnswerPosition: 0,
+            options: ["The Pennines", "The Cotswolds", "The Chilterns"]
+        },
+        {
+            question: "What’s the name of England’s longest lake?",
+            correctAnswerPosition: 2,
+            options: ["Ullswater", "Loch Ness", "Lake Windermere"]
+        },
+        {
+            question: "What is the name of the temple situated on the Acropolis in Athens, Greece, originally dedicated to the Greek goddess, Athena?",
+            correctAnswerPosition: 0,
+            options: ["The Parthenon", "The Erechtheion", "The Temple of Olympian Zeus"]
+        },
+        {
+            question: "What is the world’s longest man-made structure with an official length of 13,170.7 miles?",
+            correctAnswerPosition: 1,
+            options: ["The Trans-Siberian Railway", "The Great Wall of China", "The Grand Canal of China"]
+        },
+        {
+            question: "In what country would you find one of the largest religious monuments in the world which was built in the twelfth century and whose name translated means ‘City of Temples’?",
+            correctAnswerPosition: 0,
+            options: ["Angkor Wat in Cambodia", "Borobudur in Indonesia", "Potala Palace in Tibet"]
+        },
+        {
+            question: "Which dormant volcano is the highest peak on the African continent at 5895 metres in height?",
+            correctAnswerPosition: 1,
+            options: ["Mount Kenya in Kenya", "Mount Kilimanjaro in Tanzania", "Toubkal in Morocco"]
+        },
+        {
+            question: "What mode of transport does Greta Thunberg refuse to travel by?",
+            correctAnswerPosition: 1,
+            options: ["Bus", "Aeroplane", "Car"]
+        },
+        {
+            question: "What does unprecedented mean?",
+            correctAnswerPosition: 2,
+            options: ["A present returned to the owner", "Unexpected", "Not done or experienced before"]
+        },
+        {
+            question: "Climate shows what in the weather?",
+            correctAnswerPosition: 2,
+            options: ["Predictions", "Changes", "Patterns"]
+        },
+        {
+            question: "Climate zones are regions of the world that have similar what?",
+            correctAnswerPosition: 2,
+            options: ["Plants", "Animals", "Temperatures and Precipitation"]
+        },
+        {
+            question: "The man who mapped and classified world climate was called what?",
+            correctAnswerPosition: 2,
+            options: ["Joseph Banks", "Douglas Mawson", "Wladimir Köppen"]
+        },
+        {
+            question: "Which of the following is not a climate zone of the world?",
+            correctAnswerPosition: 1,
+            options: ["Warm temperate", "Stormy", "Tropical"]
+        },
+        {
+            question: "Which climate zone is found on either side of the equator and is hot and wet all year?",
+            correctAnswerPosition: 0,
+            options: ["Tropical", "Cool Temperate", "Dry"]
+        },
+        {
+            question: "Which climate zone is found at the top and base of the earth, and has very cold winters but little rainfall?",
+            correctAnswerPosition: 2,
+            options: ["Dry", "Tropical", "Polar"]
+        },
+        {
+            question: "What is the primary language spoken in Brazil?",
+            correctAnswerPosition: 0,
+            options: ["Portuguese", "Spanish", "French"]
+        },
+        {
+            question: "Which planet is known as the 'Red Planet'?",
+            correctAnswerPosition: 0,
+            options: ["Mars", "Jupiter", "Saturn"]
+        },
+        {
+            question: "What is the largest mammal in the world?",
+            correctAnswerPosition: 0,
+            options: ["Blue Whale", "African Elephant", "Giraffe"]
+        },
+        {
+            question: "Which country is famous for inventing the sport of cricket?",
+            correctAnswerPosition: 0,
+            options: ["England", "Australia", "India"]
+        }
+    ]
 };
-
 let selectedQuestions = [];
 let currentQuestionIndex = 0;
 let score = 0;
